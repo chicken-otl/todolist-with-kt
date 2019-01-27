e2239fc4ee5ade0910f793aa304aa1a5ee7f969f
yuko <ysugawara0220@gmail.com>
2019-01-27 20:35:20 +0900

    first commit

---
+++ src/main/kotlin/io/github/chickenotl/todolistwithkt/TodolistWithKtApplication.kt
@@ -0,0 +1,12 @@
+package io.github.chickenotl.todolistwithkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodolistWithKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodolistWithKtApplication>(*args)
+}
+
---
+++ src/test/kotlin/io/github/chickenotl/todolistwithkt/TodolistWithKtApplicationTests.kt
@@ -0,0 +1,17 @@
+package io.github.chickenotl.todolistwithkt
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TodolistWithKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
